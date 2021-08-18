8673d246fb70870b6b041b19769cf0bd1c7a0d3f
Ruaidhri O'Neill <Ruaidhri2006@gmail.com>
2021-08-18 21:57:21 +0100

    Initial Commit

---
+++ SwiftUI-Bullseye/SwiftUI_BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BullseyeApp.swift
+//  SwiftUI-Bullseye
+//
+//  Created by Ruaidhrí O'Neill on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
