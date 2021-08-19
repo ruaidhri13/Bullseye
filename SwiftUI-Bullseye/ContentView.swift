8320b7f4e9fd3f7c76de2a3385a8c109eded7421
Ruaidhri O'Neill <Ruaidhri2006@gmail.com>
2021-08-19 10:41:43 +0100

    Added Slider and Button

--- SwiftUI-Bullseye/ContentView.swift
+++ SwiftUI-Bullseye/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Hit Me!")
+            })
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
