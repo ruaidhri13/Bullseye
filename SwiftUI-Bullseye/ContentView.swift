e456244dcf81b0df552e6e6647baf00d8b18eef1
Ruaidhri O'Neill <Ruaidhri2006@gmail.com>
2021-08-19 17:19:57 +0100

    Added alertIsVisible and sliderValue

--- SwiftUI-Bullseye/ContentView.swift
+++ SwiftUI-Bullseye/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+
+    @State private var sliderValue: Double = 50.0
+    
     var body: some View {
         VStack(spacing: 20) {
             Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4)
+                .font(.footnote)
+            
             Text("89")
-            HStack {
+                .kerning(-1)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack(spacing: 25) {
                 Text("1")
-                Slider(value: .constant(50), in: 1.0...100.0)
+                    .bold()
+                
+                Slider(value: $sliderValue, in: 1...100)
+                
                 Text("100")
-            }
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Text("Hit Me!")
+                    .bold()
+            } .padding(25)
+            
+            Button(action: {
+                self.alertIsVisible = true
+            }, label: {
+                Text("HIT ME")
+            })
+            .alert(isPresented: $alertIsVisible, content: {
+                
+                let roundedValue: Int = Int(self.sliderValue.rounded())
+                
+                return Alert(title: Text("Hello there!"),
+                             message: Text("The value of the slider is \(roundedValue)"),
+                             dismissButton: .default(Text("Close this alert")))
             })
         }
     }
